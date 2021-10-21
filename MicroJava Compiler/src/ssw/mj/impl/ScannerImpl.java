63509f20fb7918944d4679dd2be9e545deec3fd4
Rudolf Garstenauer <rgarstenauer@yahoo.com>
2021-10-21 10:37:10 +0200

    Added the Todos

--- MicroJava Compiler/src/ssw/mj/impl/ScannerImpl.java
+++ MicroJava Compiler/src/ssw/mj/impl/ScannerImpl.java
@@ -14,11 +14,45 @@ public final class ScannerImpl extends Scanner {
     }
 
     /**
-     * Returns next token. To be used by parser.
-     */
+     * Returns next token. To be used by parser. */
     @Override
     public Token next() {
         // TODO
     	return null;
     }
+
+    /*
+    Reads the next input character and stores it in the ch field
+    or EOF at end of file
+    Recognizes line breaks: LF and CR LF
+    Keeps the position in the fields line and col  */
+    void nextCh(){
+        //TODO
+    }
+
+    /*
+    Read an identifier
+    Recognizes keywords (HashMap String -->Token.Kind) */
+    void readName(Token t){
+        //TODO
+    }
+
+    //Reads a number
+    void readNumber(Token t){
+        //TODO
+    }
+
+    //Reads a character constant
+    void readCharConst(Token t){
+        //TODO
+    }
+
+    /*
+    –Überliest geschachtelte Kommentare
+    –ch enthält anschließend das Zeichen nach dem Kommentar
+     */
+    void skipComment(Token t){
+        //TODO
+    }
+
 }
