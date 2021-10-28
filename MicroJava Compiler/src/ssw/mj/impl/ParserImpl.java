c70a10775b8f573003bf2407bd73bc871cf05e8b
Rudolf Garstenauer <rgarstenauer@yahoo.com>
2021-10-28 19:15:42 +0200

    Program() & ConstDecl()

--- MicroJava Compiler/src/ssw/mj/impl/ParserImpl.java
+++ MicroJava Compiler/src/ssw/mj/impl/ParserImpl.java
@@ -4,7 +4,8 @@ import ssw.mj.Parser;
 import ssw.mj.Scanner;
 import ssw.mj.Token.Kind;
 
-import static ssw.mj.Token.Kind.eof;
+import static ssw.mj.Errors.Message.TOKEN_EXPECTED;
+import static ssw.mj.Token.Kind.*;
 
 public final class ParserImpl extends Parser {
 
@@ -12,7 +13,6 @@ public final class ParserImpl extends Parser {
     public ParserImpl
     (Scanner scanner) {
         super(scanner);
-
     }
 
     /**
@@ -32,8 +32,8 @@ public final class ParserImpl extends Parser {
     }
 
     private void check (Kind expected) {
-        if (sym == expected) scan();    // recognized, so read on
-        else error("TODO");         // TODO
+        if (sym == expected) scan();
+        else error(TOKEN_EXPECTED, expected);
     }
 
     private void error (String msg) {
@@ -43,12 +43,36 @@ public final class ParserImpl extends Parser {
 
     // Program = "program" ident { ConstDecl | VarDecl | ClassDecl } "{" {MethodDecl} "}".
     private void Program(){
-        //TODO
+        check(program);
+        check(ident);
+        while(true){
+            if(sym == final_){
+                ConstDecl();
+            }else if(sym == ident){
+                VarDecl();
+            }else if( sym == class_){
+                ClassDecl();
+            }else{
+                break;
+            }
+        }
+        check(lbrace);
+        while(sym == ident || sym == void_) {
+            MethodDecl();
+        }
+        check(rbrace);
     }
 
     // ConstDecl = "final" Type ident "=" ( number | charConst ) ";".
     private void ConstDecl(){
-        //TODO
+        check(final_);
+        Type();
+        check(ident);
+        check(assign);
+        if(sym == number || sym == charConst){
+            scan();
+        }
+        check(semicolon);
     }
 
     // VarDecl = Type ident { "," ident } ";".
@@ -57,16 +81,21 @@ public final class ParserImpl extends Parser {
     }
 
     // ClassDecl = "class" ident "{" { VarDecl } "}".
-    private void MethodDecl(){
+    private void ClassDecl(){
         //TODO
     }
 
     // MethodDecl = ( Type | "void" ) ident "(" [ FormPars ] ")" { VarDecl } Block.
-    private void FormPars(){
+    private void MethodDecl(){
         //TODO
     }
 
     // FormPars = Type ident { "," Type ident } [ ppperiod ].
+    private void FormPars(){
+        //TODO
+    }
+
+    // Type = ident [ "[" "]" ].
     private void Type(){
         //TODO
     }
